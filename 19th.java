3f1447e770bdaaf28c2600d411c20cd278329556
Harsh Mehta <138463643+Harsh-Mehta-15@users.noreply.github.com>
2025-07-06 18:14:19 +0530

    Update 19th.java

--- 19th.java
+++ 19th.java
@@ -1,1 +1,23 @@
-// 19.WAP to sort an Array using Bucket sort. 
+// Checking balanced brackets
+
+class Nineteen {
+    public static void main(String[] args) {
+        String str = "{{}[}";
+        char ch = ' ';
+        int count = 0;
+        for(int i = 0;i<str.length();i++){
+            ch = str.charAt(i);
+            if(ch=='{' || ch=='[' || ch=='('){
+                count++;
+            }
+            if(ch=='}' || ch==']' || ch==')'){
+                count--;
+            }
+        }
+        if(count == 0){
+            System.out.print("Correct");
+        } else {
+            System.out.print("Not Correct");   
+        }
+    }
+}
